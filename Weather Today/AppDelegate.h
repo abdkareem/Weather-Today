dda858e18e7bd8350d6ee6d7aec0f318c889b70e
root <abd.khaja@gmail.com>
2017-02-08 22:05:35 -0800

    Initial Commit

---
+++ Weather Today/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Weather Today
+//
+//  Created by Taiyaba Sultana on 2/8/17.
+//  Copyright © 2017 Abdul Kareem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Weather Today/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Weather Today
+//
+//  Created by Taiyaba Sultana on 2/8/17.
+//  Copyright © 2017 Abdul Kareem. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
